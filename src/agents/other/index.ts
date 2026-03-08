544f624079c2a9a8fad660ebb7009798b9ad0ace
Miles F <miles.fritzmather@gmail.com>
2026-03-08 17:17:03 -0500

    should stop wrangler.jsonc changes and added ci checks for github

--- src/agents/other/index.ts
+++ src/agents/other/index.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 import { defineAgent, defineTool } from "../../shared/agent-creator";
 
+async function getQuote(page: number) {
+	const response = await fetch(
+		`https://gutenberg.org/cache/epub/1342/pg1342.txt`,
+	);
+	const text = await response.text();
+	const lines = text.split("\n");
+	return lines[page];
+}
+
 export const { AgentClass: OtherServer, metadata } = defineAgent({
 	name: "Other Server",
 	version: "1.0.0",
@@ -8,10 +17,19 @@ export const { AgentClass: OtherServer, metadata } = defineAgent({
 	url_prefix: "/other",
 	tools: [
 		defineTool({
-			name: "foo",
-			inputSchema: { name: z.number() },
-			function: async ({ name }) => ({
-				content: [{ type: "text", text: name.toExponential().toString() }],
+			name: "randomNumber",
+			inputSchema: { range: z.number().min(0).max(100) },
+			function: async ({ range }) => ({
+				content: [
+					{ type: "text", text: Math.floor(Math.random() * range).toString() },
+				],
+			}),
+		}),
+		defineTool({
+			name: "getQuote",
+			inputSchema: { page: z.number() },
+			function: async ({ page }) => ({
+				content: [{ type: "text", text: await getQuote(page) }],
 			}),
 		}),
 		defineTool({
